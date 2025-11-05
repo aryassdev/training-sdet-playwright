998ff30450938c3c159698ee1c386b7414c35c07
Arya W <arya@softwareseni.com>
2025-11-05 15:39:57 +0700

    test: add user can login test

---
+++ tests/saucedemo.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect} from '@playwright/test';
+
+test('user can log in to saucedemo', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+
+    await page.locator('[data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
+});
