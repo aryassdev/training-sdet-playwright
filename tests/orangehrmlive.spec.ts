9288cd74551589bd981ebba8264d21df65eed56c
Arya W <arya@softwareseni.com>
2025-11-05 17:18:10 +0700

    test: add parent locators

--- tests/orangehrmlive.spec.ts
+++ tests/orangehrmlive.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('user can log in to orangehrmlive', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('input[name="username"]').fill('Admin');
-    await page.locator('input[name="password"]').fill('admin123');
-    await page.locator('button[type="submit"]').click();
+    await page.locator('.orangehrm-login-form input[name="username"]').fill('Admin');
+    await page.locator('.orangehrm-login-form input[name="password"]').fill('admin123');
+    await page.locator('.orangehrm-login-form button[type="submit"]').click();
 
     await expect(page.url()).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
 });
@@ -13,9 +13,9 @@ test('user can log in to orangehrmlive', async ({ page }) => {
 test('user can not login to orangehrmlive with invalid credentials', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('input[name="username"]').fill('some_user');
-    await page.locator('input[name="password"]').fill('some_password');
-    await page.locator('button[type="submit"]').click();
+    await page.locator('.orangehrm-login-form input[name="username"]').fill('some_user');
+    await page.locator('.orangehrm-login-form input[name="password"]').fill('some_password');
+    await page.locator('.orangehrm-login-form button[type="submit"]').click();
 
     await expect(page.locator('.oxd-alert-content--error .oxd-alert-content-text')).toHaveText('Invalid credentials');
 })
@@ -23,24 +23,24 @@ test('user can not login to orangehrmlive with invalid credentials', async ({ pa
 test('user can not login with empty credentials', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('input[name="username"]').fill('');
-    await page.locator('input[name="password"]').fill('');
+    await page.locator('.orangehrm-login-form input[name="username"]').fill('');
+    await page.locator('.orangehrm-login-form input[name="password"]').fill('');
 
-    await page.locator('button[type="submit"]').click();
+    await page.locator('.orangehrm-login-form button[type="submit"]').click();
 
-    await expect(page.locator('.oxd-input-group__message').nth(0)) .toHaveText('Required');
-    await expect(page.locator('.oxd-input-group__message').nth(1)) .toHaveText('Required');
+    await expect(page.locator('.orangehrm-login-form .oxd-input-group__message').nth(0)) .toHaveText('Required');
+    await expect(page.locator('.orangehrm-login-form .oxd-input-group__message').nth(1)) .toHaveText('Required');
 });
 
 test('user can log out from orangehrmlive', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('input[name="username"]').fill('Admin');
-    await page.locator('input[name="password"]').fill('admin123');
-    await page.locator('button[type="submit"]').click();
+    await page.locator('.orangehrm-login-form input[name="username"]').fill('Admin');
+    await page.locator('.orangehrm-login-form input[name="password"]').fill('admin123');
+    await page.locator('.orangehrm-login-form button[type="submit"]').click();
 
-    await page.locator('.oxd-userdropdown-tab .oxd-userdropdown-name').click();
-    await page.locator('a[href="/web/index.php/auth/logout"]').click();
+    await page.locator('.oxd-topbar-header .oxd-userdropdown-tab .oxd-userdropdown-name').click();
+    await page.locator('.oxd-topbar-header a[href="/web/index.php/auth/logout"]').click();
 
     await expect(page.url()).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 });
--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -3,9 +3,9 @@ import { test, expect} from '@playwright/test';
 test('user can log in to saucedemo', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
 
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-container"] [data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="login-container"] [data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-container"] [data-test="login-button"]').click();
 
     await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
 });
@@ -13,32 +13,32 @@ test('user can log in to saucedemo', async ({ page }) => {
 test('user cannot log in with invalid credentials', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     
-    await page.locator('[data-test="username"]').fill('wrong_user');
-    await page.locator('[data-test="password"]').fill('wrong_password');
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-container"] [data-test="username"]').fill('wrong_user');
+    await page.locator('[data-test="login-container"] [data-test="password"]').fill('wrong_password');
+    await page.locator('[data-test="login-container"] [data-test="login-button"]').click();
 
-    await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match any user in this service');
+    await expect(page.locator('[data-test="login-container"] [data-test="error"]')).toContainText('Username and password do not match any user in this service');
 });
 
 test('user cannot log in with empty username', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     
-    await page.locator('[data-test="username"]').fill('');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-container"] [data-test="username"]').fill('');
+    await page.locator('[data-test="login-container"] [data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-container"] [data-test="login-button"]').click();
 
-    await expect(page.locator('[data-test="error"]')).toContainText('Username is required');
+    await expect(page.locator('[data-test="login-container"] [data-test="error"]')).toContainText('Username is required');
 });
 
 test('user can log out', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
 
-    await page.locator('[data-test="username"]').fill('standard_user');
-    await page.locator('[data-test="password"]').fill('secret_sauce');
-    await page.locator('[data-test="login-button"]').click();
+    await page.locator('[data-test="login-container"] [data-test="username"]').fill('standard_user');
+    await page.locator('[data-test="login-container"] [data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-container"] [data-test="login-button"]').click();
     
-    await page.locator('#react-burger-menu-btn').click();
-    await page.locator('[data-test="logout-sidebar-link"]').click();
+    await page.locator('#menu_button_container #react-burger-menu-btn').click();
+    await page.locator('#menu_button_container [data-test="logout-sidebar-link"]').click();
     
     await expect(page.url()).toBe('https://www.saucedemo.com/');
 });
