cc6b8be576434b9d571f285d83fb39ed6980b698
Arya W <arya@softwareseni.com>
2025-11-05 16:31:32 +0700

    test: add user can not login test

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -9,3 +9,23 @@ test('user can log in to saucedemo', async ({ page }) => {
 
     await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html');
 });
+
+test('user cannot log in with invalid credentials', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    
+    await page.locator('[data-test="username"]').fill('wrong_user');
+    await page.locator('[data-test="password"]').fill('wrong_password');
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match any user in this service');
+});
+
+test('user cannot log in with empty username', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    
+    await page.locator('[data-test="username"]').fill('');
+    await page.locator('[data-test="password"]').fill('secret_sauce');
+    await page.locator('[data-test="login-button"]').click();
+
+    await expect(page.locator('[data-test="error"]')).toContainText('Username is required');
+});
