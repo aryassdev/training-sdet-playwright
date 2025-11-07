a083a3dc0990dc4f219a60b25988331aad857cc2
Arya W <arya@softwareseni.com>
2025-11-07 09:53:16 +0700

    test: remove parent locator from orangehrm spec

--- tests/orangehrmlive.spec.ts
+++ tests/orangehrmlive.spec.ts
@@ -3,9 +3,9 @@ import { test, expect } from '@playwright/test';
 test('User should be able to Login with valid credentials and verify the correct URL after logged in', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('.orangehrm-login-form input[name="username"]').fill('Admin');
-    await page.locator('.orangehrm-login-form input[name="password"]').fill('admin123');
-    await page.locator('.orangehrm-login-form button[type="submit"]').click();
+    await page.locator('input[name="username"]').fill('Admin');
+    await page.locator('input[name="password"]').fill('admin123');
+    await page.locator('button[type="submit"]').click();
 
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
 });
@@ -13,34 +13,34 @@ test('User should be able to Login with valid credentials and verify the correct
 test('User should unable to Login with invalid credentials and verify the “Invalid credentials” message appears', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('.orangehrm-login-form input[name="username"]').fill('some_user');
-    await page.locator('.orangehrm-login-form input[name="password"]').fill('some_password');
-    await page.locator('.orangehrm-login-form button[type="submit"]').click();
+    await page.locator('input[name="username"]').fill('some_user');
+    await page.locator('input[name="password"]').fill('some_password');
+    await page.locator('button[type="submit"]').click();
 
-    await expect(page.locator('.oxd-alert-content--error .oxd-alert-content-text')).toHaveText('Invalid credentials');
+    await expect(page.locator('.oxd-alert-content-text')).toHaveText('Invalid credentials');
 })
 
 test('User should unable to Login with empty fields and verify the “required” message appears', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('.orangehrm-login-form input[name="username"]').fill('');
-    await page.locator('.orangehrm-login-form input[name="password"]').fill('');
+    await page.locator('input[name="username"]').fill('');
+    await page.locator('input[name="password"]').fill('');
 
-    await page.locator('.orangehrm-login-form button[type="submit"]').click();
+    await page.locator('button[type="submit"]').click();
 
-    await expect(page.locator('.orangehrm-login-form .oxd-input-group__message').nth(0)) .toHaveText('Required');
-    await expect(page.locator('.orangehrm-login-form .oxd-input-group__message').nth(1)) .toHaveText('Required');
+    await expect(page.locator('.oxd-input-group__message').nth(0)).toHaveText('Required');
+    await expect(page.locator('.oxd-input-group__message').nth(1)).toHaveText('Required');
 });
 
 test('User should be able to Logout and verify the correct URL after logged out', async ({ page }) => {
     await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 
-    await page.locator('.orangehrm-login-form input[name="username"]').fill('Admin');
-    await page.locator('.orangehrm-login-form input[name="password"]').fill('admin123');
-    await page.locator('.orangehrm-login-form button[type="submit"]').click();
+    await page.locator('input[name="username"]').fill('Admin');
+    await page.locator('input[name="password"]').fill('admin123');
+    await page.locator('button[type="submit"]').click();
 
-    await page.locator('.oxd-topbar-header .oxd-userdropdown-tab .oxd-userdropdown-name').click();
-    await page.locator('.oxd-topbar-header a[href="/web/index.php/auth/logout"]').click();
+    await page.locator('.oxd-userdropdown-tab .oxd-userdropdown-name').click();
+    await page.locator('a[href="/web/index.php/auth/logout"]').click();
 
     await expect(page).toHaveURL('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
 });
